aa0cd769234d06355d54068f9844e060811c9e31
Daniel Gao <15221024927@163.com>
2024-09-24 22:30:46 +0800

    class
    Signed-off-by: Daniel Gao <15221024927@163.com>

--- class继承关系.py
+++ class继承关系.py
@@ -7,3 +7,9 @@ class C(A, B):
 print('类A的所有父类:', A.__bases__)
 print('类B的所有父类:', B.__bases__)
 print('类C的所有父类:', C.__bases__)
+
+print('类A的所有子类:', A.__subclasses__())
+print('类B的所有子类:', B.__subclasses__())
+print('类C的所有子类:', C.__subclasses__())
+
+print('类object的所有子类:', object.__subclasses__())
