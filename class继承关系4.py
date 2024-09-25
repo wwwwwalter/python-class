6d7b6db29d36e3ace10fa3d9a7515bc602cc9b44
Daniel Gao <15221024927@163.com>
2024-09-25 09:45:06 +0800

    class
    Signed-off-by: Daniel Gao <15221024927@163.com>

--- class继承关系4.py
+++ class继承关系4.py
@@ -16,11 +16,22 @@ obj = MyClass()
 # 获取对象的属性名称列表
 attributes = dir(obj)
 
-# 遍历属性名称，判断是属性还是方法
+# 方法一：遍历属性名称，判断是属性还是方法
 for attr_name in attributes:
     attr = getattr(obj, attr_name)
 
     if callable(attr):  # 检查是否为可调用对象（方法）
+        print(f"{attr_name} 是方法")
+    else:
+        print(f"{attr_name} 是属性")
+
+print("****************************************")
+
+# 方法二：遍历属性名称，判断是属性还是方法
+for attr_name in attributes:
+    attr = getattr(obj, attr_name)
+
+    if hasattr(attr, "__call__"):
         print(f"{attr_name} 是方法")
     else:
         print(f"{attr_name} 是属性")
