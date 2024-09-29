db5405772310c850fbf9e1a8f8c02b8d85d49139
Daniel Gao <15221024927@163.com>
2024-09-30 00:14:39 +0800

    print default formal parameter
    Signed-off-by: Daniel Gao <15221024927@163.com>

---
+++ function/打印某个函数形参列表里的默认值参数.py
@@ -0,0 +1,8 @@
+def dis_str(str1,str2 = "python2",str3 = "python3"):
+    print("str1:",str1)
+    print("str2:",str2)
+    print("str3:",str3)
+
+
+if __name__ == "__main__":
+    print(dis_str.__defaults__) # 打印一个元组出来
